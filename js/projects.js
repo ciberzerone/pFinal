c39732e5e53eb54b310a70c0bccef391e2e2291a
ciberzerone <gianmarcobeltran@gmail.com>
2024-07-03 05:01:40 +0200

    d17

--- js/projects.js
+++ js/projects.js
@@ -1,7 +1,7 @@
 window.addEventListener("load", async () => {
     try {
         const response = await fetch(
-            "https://raw.githubusercontent.com/ciberzerone/pFinal/main/projects/0_db.json"
+            "https://raw.githubusercontent.com/ironhack-jc/mid-term-api/main/projects"
         );
         const data = await response.json();
 
